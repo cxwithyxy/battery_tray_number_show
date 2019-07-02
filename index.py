9d5976359c674837b9868972ba057d413106b875
chenxuan <523764720@qq.com>
2019-07-02 15:37:01 +0800

    现在会动了

--- index.py
+++ index.py
@@ -1,33 +1,40 @@
+from PIL import Image, ImageDraw, ImageFont
 import pystray
 import _thread
+import time
 
-icon = pystray.Icon('test name')
+icon = pystray.Icon('battery_level_show')
 
-from PIL import Image, ImageDraw
 
-def create_image():
-    # Generate an image and draw a pattern
+def create_icon_by_text(text):
     width = 200
     height = 200
     color1 = 0x000000
     color2 = 0xFFFFFF
     image = Image.new('RGB', (width, height), color1)
     dc = ImageDraw.Draw(image)
-    dc.rectangle(
-        (width // 2, 0, width, height // 2),
-        fill=color2)
-    dc.rectangle(
-        (0, height // 2, width // 2, height),
-        fill=color2)
+    dc.text((0,0),text,font=ImageFont.truetype('C:/Windows/Fonts/Arial.ttf',190),fill = color2)
 
     return image
 
-icon.icon = create_image()
+icon.icon = create_icon_by_text("99")
 
 def do():
     icon.run()
 
 
+battery_level = 0
+
+def get_battery_level():
+    global battery_level
+    while(True):
+        icon.icon = create_icon_by_text(str(battery_level))
+        time.sleep(1)
+        battery_level += 1
+
+
+
 _thread.start_new_thread(do,())
+_thread.start_new_thread(get_battery_level,())
 
 input("121212")
