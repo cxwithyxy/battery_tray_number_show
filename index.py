29bf2bdd0e026a3083648539f6ed2e2a18fc2102
chenxuan <523764720@qq.com>
2019-07-02 15:23:52 +0800

    可以多线程控制托盘

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import pystray
+import _thread
 
 icon = pystray.Icon('test name')
 
@@ -6,8 +7,8 @@ from PIL import Image, ImageDraw
 
 def create_image():
     # Generate an image and draw a pattern
-    width = 20
-    height = 20
+    width = 200
+    height = 200
     color1 = 0x000000
     color2 = 0xFFFFFF
     image = Image.new('RGB', (width, height), color1)
@@ -23,4 +24,10 @@ def create_image():
 
 icon.icon = create_image()
 
-icon.run()
+def do():
+    icon.run()
+
+
+_thread.start_new_thread(do,())
+
+input("121212")
