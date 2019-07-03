3be48fed02304731eb1b21becc1f54c2096e408b
chenxuan <523764720@qq.com>
2019-07-03 10:17:38 +0800

    原来是pyinstaller 与 multiprocessing 冲突, 通过freeze_support函数解决了

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import Tray_display
-from multiprocessing import Pool 
+from multiprocessing import Pool, freeze_support
 import sys
 
 def exit_app():
@@ -12,8 +12,9 @@ def tray_display_multiprocessing():
     tray_display.run()
 
 if __name__ == "__main__":
+    freeze_support()
     po = Pool(1)
 
     po.apply_async(tray_display_multiprocessing)
 
-    input("battery_tray_number_show start")
+    input("battery_tray_number_show start \n")
